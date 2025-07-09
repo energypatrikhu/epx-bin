36f2c9df2aaaa7ed87578c780439160bbb84242f
energypatrikhu <energypatrikhu@proton.me>
2025-07-09 16:56:25 +0200

    Add autocomplete and autoscripts loading functionality

--- aliases.sh
+++ aliases.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+_load_aliases() {
+  for element in "$1"/*; do
+    if [[ -d "$element" ]]; then
+      _load_aliases "$element"
+      continue
+    fi
+
+    if [[ -f "$element" ]] && [[ "$element" == *.sh ]]; then
+      if [[ "$element" == *"_alias.sh" ]]; then
+        source "$element"
+      fi
+    fi
+  done
+}
+_load_aliases "$EPX_HOME/commands"
+
 alias ff='fastfetch'
 
 alias c='clear'
--- autoscripts.sh
+++ autoscripts.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-_load_autocomplete() {
+_load_auto() {
   for element in "$1"/*; do
     if [[ -d "$element" ]]; then
-      _load_autocomplete "$element"
+      _load_auto "$element"
       continue
     fi
 
     if [[ -f "$element" ]] && [[ "$element" == *.sh ]]; then
-      if [[ "$element" == *"_autocomplete.sh" ]]; then
+      if [[ "$element" == *"_auto."*.sh ]]; then
         source "$element"
       fi
     fi
   done
 }
-_load_autocomplete "$EPX_HOME/commands"
+_load_auto "$EPX_HOME/commands"
