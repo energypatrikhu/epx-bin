e08c55b113199e3dd25952d53ef439ae1d629247
energypatrikhu <energypatrikhu@proton.me>
2025-07-09 17:57:06 +0200

    Refactor Minecraft autocomplete and helpers scripts to ensure configuration loading only occurs if the config file exists

--- commands/game-servers/minecraft/_autocomplete.sh
+++ commands/game-servers/minecraft/_autocomplete.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-source "$EPX_HOME/commands/game-servers/minecraft/_helpers.sh"
+if [[ -f "$EPX_HOME/.config/minecraft.config" ]]; then
+  source "$EPX_HOME/commands/game-servers/minecraft/_helpers.sh"
 
-__epx-mc-list-configs() {
-  _autocomplete "$(__epx-mc-get-configs "$1")"
-}
-complete -F __epx-mc-list-configs mc
+  __epx-mc-list-configs() {
+    _autocomplete "$(__epx-mc-get-configs "$1")"
+  }
+  complete -F __epx-mc-list-configs mc
 
-__epx-mc-list-configs-examples() {
-  _autocomplete "$(__epx-mc-get-configs-examples "$1")"
-}
-complete -F __epx-mc-list-configs-examples mc.create
+  __epx-mc-list-configs-examples() {
+    _autocomplete "$(__epx-mc-get-configs-examples "$1")"
+  }
+  complete -F __epx-mc-list-configs-examples mc.create
+fi
