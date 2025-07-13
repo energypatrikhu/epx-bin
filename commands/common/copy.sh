d5c5b78435f950421ec99a5b7a3ebef5c326bf23
energypatrikhu <energypatrikhu@proton.me>
2025-07-13 07:51:16 +0200

    Enhance copy and move scripts with input validation and confirmation prompts

--- commands/common/copy.sh
+++ commands/common/copy.sh
@@ -1,8 +1,47 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-  echo -e "No input files"
+  echo "No input files"
   exit 1
 fi
 
-time rsync -rxzvuahP --stats "${@}"
+local input="$1"
+local output="$2"
+local rest="${@:3}"
+
+if [ input = output ]; then
+  echo "Input and output cannot be the same: $input"
+  exit 1
+fi
+
+if [ input = "/" ] | [ "$input" = "/*" ]; then
+  echo "Input cannot be the root directory: $input"
+  exit 1
+fi
+
+# Prompt for confirmation before copying
+read -p "Are you sure you want to copy '$input' to '$output'? [y/N] " confirm
+case "$confirm" in
+[yY][eE][sS] | [yY]) ;;
+*)
+  echo "Cancelled."
+  exit 1
+  ;;
+esac
+
+# strip ending slash from input if it exists
+local stripped_input="${input%/}"
+
+# Check if input exists
+if [ ! -e "$stripped_input" ]; then
+  echo "Input file or directory does not exist: $stripped_input"
+  exit 1
+fi
+
+# Check if output exists
+if [ ! -e "$output" ]; then
+  mkdir -p "$(dirname "$output")"
+fi
+
+# Use rsync to copy files
+time rsync -rxzvuah --stats --inplace --info=progress2 "$stripped_input" "$output" $rest
--- commands/common/move.sh
+++ commands/common/move.sh
@@ -1,8 +1,49 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-  echo -e "No input files"
+  echo "No input files"
   exit 1
 fi
 
-time rsync -rxzvuahP --remove-source-files --stats "${@}" && find "${1}" -type d -empty -delete
+local input="$1"
+local output="$2"
+local rest="${@:3}"
+
+if [ input = output ]; then
+  echo "Input and output cannot be the same: $input"
+  exit 1
+fi
+
+if [ input = "/" ] | [ "$input" = "/*" ]; then
+  echo "Input cannot be the root directory: $input"
+  exit 1
+fi
+
+# Prompt for confirmation before moving
+read -p "Are you sure you want to move '$input' to '$output'? [y/N] " confirm
+case "$confirm" in
+[yY][eE][sS] | [yY]) ;;
+*)
+  echo "Cancelled."
+  exit 1
+  ;;
+esac
+
+# strip ending slash from input if it exists
+local stripped_input="${input%/}"
+
+# Check if input exists
+if [ ! -e "$stripped_input" ]; then
+  echo "Input file or directory does not exist: $stripped_input"
+  exit 1
+fi
+
+# Check if output exists
+if [ ! -e "$output" ]; then
+  mkdir -p "$(dirname "$output")"
+fi
+
+# Use rsync to copy files
+time rsync -rxzvuah --stats --inplace --info=progress2 "$stripped_input" "$output" $rest
+
+rm -rf "$input"
